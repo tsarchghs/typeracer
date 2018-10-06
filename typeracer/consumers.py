83bd8742a1de362b3cb6aee55857c10a60288588
gjergjk71 <gjergjk71@gmail.com>
2018-10-06 20:01:03 +0200

    Created player object when connected

--- typeracer/consumers.py
+++ typeracer/consumers.py
@@ -1,10 +1,12 @@
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from .models import Race,Player
 import json
 
 class TypeRacerConsumer(WebsocketConsumer):
 	def connect(self):
 		self.race_id = self.scope['url_route']['kwargs']['race_id']
+		self.race = Race.objects.get(pk=self.race_id)
 		async_to_sync(self.channel_layer.group_add)(
 			self.race_id,
 			self.channel_name
@@ -18,14 +20,24 @@ class TypeRacerConsumer(WebsocketConsumer):
 			}
 		)
 	def receive(self,text_data):
-		print(self.race_id)
 		text_data_json = json.loads(text_data);
-		async_to_sync(self.channel_layer.group_send)(
-			self.race_id,
-			{
-				"type":"shareEvent",
-				"message":text_data_json
-			})
+		try:
+			if text_data_json["create_player"]:
+				player_id = text_data_json["player_id"]
+				text_length = text_data_json["text_length"]
+
+				self.player = Player.objects.create(race=self.race,
+													player_id=player_id,
+													characters_typed=0,
+													text_length=text_length)
+		except KeyError:
+			print(text_data_json)
+			async_to_sync(self.channel_layer.group_send)(
+				self.race_id,
+				{
+					"type":"shareEvent",
+					"message":text_data_json
+				})
 	def shareEvent(self,event):
 		self.send(text_data=json.dumps(event))
 	def disconnect(self,close_code):
