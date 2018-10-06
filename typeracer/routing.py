8fd2346bd6e0e39775db5a8e3644cccdec2ff97a
gjergjk71 <gjergjk71@gmail.com>
2018-10-06 18:49:14 +0200

    Added race_id url parameter

--- typeracer/routing.py
+++ typeracer/routing.py
@@ -1,14 +1,15 @@
 from channels.routing import ProtocolTypeRouter
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.conf.urls import url
+from django.urls import path
 from . import consumers
+from django.conf.urls import url
 
 application = ProtocolTypeRouter({
 	    'websocket': AuthMiddlewareStack(
         URLRouter(
         	[
-				url(r'^ws/TypeRacer/$', consumers.TypeRacerConsumer),
+        	url(r'^ws/TypeRacer/(?P<race_id>[^/]+)/$', consumers.TypeRacerConsumer),
 			]
         )
     ),
