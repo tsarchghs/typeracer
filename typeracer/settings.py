bc1001068c5e9e0ed2ba7daf387a7f4fb3b2b9eb
gjergjk71 <gjergjk71@gmail.com>
2018-10-06 18:49:28 +0200

    Static files configurations and added typeracer to INSTALLED APPS

--- typeracer/settings.py
+++ typeracer/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "typeracer",
     "channels",
     "channels_redis",
     'django.contrib.admin',
@@ -128,4 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_URL = '/static/'
