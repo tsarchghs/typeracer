0efee00f5641b1d6215035a69c98b8b189cee679
gjergjk71 <gjergjk71@gmail.com>
2018-10-06 20:00:23 +0200

    Created Race and Player model

---
+++ typeracer/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Race(models.Model):
+	choices = (("open","open"),("closed","closed"),)
+	status = models.CharField(max_length=6,choices=choices)
+
+class Player(models.Model):
+	race = models.ForeignKey(Race,on_delete=models.CASCADE)
+	player_id = models.IntegerField()
+	characters_typed = models.IntegerField()
+	text_length = models.IntegerField()
