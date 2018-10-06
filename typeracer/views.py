1fd8ba30ae6026b6e7ea4389bee47628c52e3a10
gjergjk71 <gjergjk71@gmail.com>
2018-10-06 20:01:35 +0200

    typeRacer view passes players to template

--- typeracer/views.py
+++ typeracer/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from .models import Race,Player
 # Create your views here.
 
 def typeRacer(request,race_id):
-	return render(request,"typeRacer.html",{"race_id":race_id})
+	race = Race.objects.filter(pk=int(race_id))
+	if not race:
+		race = Race.objects.create(status="open")
+	else:
+		race = race[0]
+	players = Player.objects.filter(race=race)
+	context = {"race_id":race_id,"players":players}
+	return render(request,"typeRacer.html",context)
