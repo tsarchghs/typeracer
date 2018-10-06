ecf71433e70f48506d7ba2e490a6a69a8abdd486
gjergjk71 <gjergjk71@gmail.com>
2018-10-06 23:47:17 +0200

    Overwrote __str__

--- typeracer/models.py
+++ typeracer/models.py
@@ -3,9 +3,12 @@ from django.db import models
 class Race(models.Model):
 	choices = (("open","open"),("closed","closed"),)
 	status = models.CharField(max_length=6,choices=choices)
+	max_players = models.IntegerField()
 
 class Player(models.Model):
 	race = models.ForeignKey(Race,on_delete=models.CASCADE)
 	player_id = models.IntegerField()
 	characters_typed = models.IntegerField()
 	text_length = models.IntegerField()
+	def __str__(self):
+		return "Player-{}".format(self.player_id)
