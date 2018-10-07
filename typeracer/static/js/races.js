de2589791e95bc4b3fea03a9f56fdf45784c9729
gjergjk71 <gjergjk71@gmail.com>
2018-10-07 11:10:26 +0200

    races.js connects to "all" socket group

--- typeracer/static/js/races.js
+++ typeracer/static/js/races.js
@@ -1,9 +1,10 @@
 
-var race_id = document.getElementById("race_id").value;
-var TypeRacerWebSocket = new WebSocket(`ws://localhost:8000/ws/TypeRacer/${race_id}/`);
 
-function addLobby(id){
-	ul = document.getElementById("lobbies_ul");
+var TypeRacerWebSocket = new WebSocket(`ws://localhost:8000/ws/TypeRacer/all/`);
+
+
+function addRace(id){
+	ul = document.getElementById("races_ul");
 	if (!document.getElementById(id)){
 		lobby_li = document.createElement("li");
 		lobby_li.id = id;
@@ -18,12 +19,20 @@ function addLobby(id){
 TypeRacerWebSocket.onmessage = function(event) {
 	json = JSON.parse(event.data);
 	console.log(json);
+	message = json["message"]
+	if (message && "update_race" in message){
+		race_players  = document.getElementById(`${message["race_id"]}_players`);
+		console.log(race_players);
+		current_players = String(Number(race_players.innerHTML.split("/")[0]) + 1);
+		max_players = race_players.innerHTML.split("/")[1];
+		race_players.innerHTML = `${current_players}/${max_players}`;
+	}
 	if ("connected_to_lobby" in json) {
-		addLobby(json["race_id"]);
+		addRace(json["race_id"]);
 	} else if ("message" in json){
 		message = json["message"];
 		if ("connected_to_lobby" in message) {
-			addLobby(message["race_id"]);
+			addRace(message["race_id"]);
 		}
 	}
 }
