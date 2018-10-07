7c28cf53963e66335dd9e27ea8102fae252e34ab
gjergjk71 <gjergjk71@gmail.com>
2018-10-07 09:23:19 +0200

    Created races view

--- typeracer/views.py
+++ typeracer/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render,redirect
 from .models import Race,Player
 # Create your views here.
 
+def races(request):
+	race_players = {}
+	races = Race.objects.all()
+	players = Player.objects.all()
+	for player in players:
+		if player.race in race_players:
+			race_players[player.race].append(player)
+		else:
+			race_players[player.race] = [player]
+	context = {"race_players":race_players}
+	return render(request,"races.html",context)
+
 def typeRacer(request,race_id):
 	race = Race.objects.filter(pk=int(race_id))
 	if not race:
